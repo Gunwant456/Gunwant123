db3a4c06b3b8acf0646bf16cb83133a0a2f5f6c5
gunwant <gunwant_manker@persistent.com>
2020-01-28 11:54:37 +0530

    this is from branch

---
+++ src/test/java/Academy/temprory.java
@@ -0,0 +1,15 @@
+package Academy;
+
+public class temprory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is for GIT test");
+		System.out.println("This is for GIT test 1");
+		System.out.println("This is for GIT test 2");
+
+		
+	}
+
+}
